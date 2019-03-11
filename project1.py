e134de551c37e4783fcd84f372a66559e4a1db20
Adam <adamthk@gmail.com>
2019-03-11 11:45:59 -0400

    Set up project

---
+++ project1.py
@@ -0,0 +1,39 @@
+__author__ = "Cameron Scott, Adam Kuniholm, and Sam Fite"
+__copyright__ = "Copyright 2019, Cameron Scott, Adam Kuniholm, and Sam Fite"
+__credits__ = ["Cameron Scott", "Adam Kuniholm", "Sam Fite"]
+__license__ = "MIT"
+
+'''
+CS 4210 Operating Systems
+Group Project 1 - CPU Scheduling Simulation
+
+• This project is due by 11:59:59 PM on Monday, March 18, 2019.
+• As per usual, your code must successfully compile/run on Submitty, 
+  which uses Ubuntu v18.04.1 LTS.
+• For Python, you must use python3, which is Python 3.6.7. 
+  Be sure to name your main Python file project1.py
+  
+== Project specifications ==
+In this first project, you will implement a rudimentary simulation of an operating
+system. The initial focus will be on processes, assumed to be resident in memory, 
+waiting to use the CPU. Memory and the I/O subsystem will not be covered in depth 
+in this project.
+  
+''' 
+
+
+'''#############################################################################
+#                                   IMPORTS                                    #
+#############################################################################'''
+import sys
+'''#############################################################################
+#                              CLASS DECLARATIONS                              #
+#############################################################################'''
+class Process:
+    pass
+
+'''#############################################################################
+#                                     MAIN                                     #
+#############################################################################'''
+if __name__ == '__main__': 
+    print("Hello, world!")
